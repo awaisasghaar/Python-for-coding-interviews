6faa159c8e8f2fb665f174c7e5d418c842836a37
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-02-27 15:52:23 +0500

    Add variables

---
+++ variables.py
@@ -0,0 +1,22 @@
+# In Python, Variables are dynamicaly typed
+
+val_1 = 1
+print('\n','The value of n is n =', val_1)
+
+val_2 = 'abcd'
+print('Strings are', val_2)
+
+# Multiple assignments
+a, b, c = 1, 5, 8
+print(a, b, c)
+
+# Increment
+a += 1
+b += 1
+c += 1
+print(a, b, c)
+
+# NONE is Null in Python(absence of value)
+a = 4
+a = None
+print(a, end=' ')
