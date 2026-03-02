801c65fabdb5cf3b92529e65451f17897be174ad
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-02 16:55:47 +0500

    Add math

---
+++ math.py
@@ -0,0 +1,8 @@
+'''Division in Python is decimal by default'''
+print(f'{100 / 2:.1f}')
+
+'''Double slash round down'''
+print(f'{100 // 2:.1f}')
+
+'''Negative numbers wil also round down'''
+print(f'{-100 // 2:.1f}')
