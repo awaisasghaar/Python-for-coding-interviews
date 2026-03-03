6e8c3f328eb6131b59dfc01d7ccf2964da919e5b
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-03 13:24:27 +0500

    Add more math

--- math.py
+++ math.py
@@ -6,3 +6,26 @@ print(f'{100 // 2:.1f}')
 
 '''Negative numbers wil also round down'''
 print(f'{-100 // 2:.1f}')
+
+# Moddling in Python
+
+print(f'{100 % 3:.0f}')
+print(f'{-100 % 3:.0f}')
+
+import math
+print(math.floor(3/ 2))
+print(math.ceil(3 / 2))
+print(f'{math.sqrt(2):.3f}')
+print(math.pow(2, 3))
+
+# Max/Min Int
+
+float('inf')
+float('-inf')
+
+'''Python numbers are infinit so they never overflow'''
+import math
+print(math.pow(2, 200))
+print(math.pow(2, 200) < float('inf'))
+print(math.pow(2, 200) < float('-inf'))
+
