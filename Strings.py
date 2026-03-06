ba35b4b79410f84863efadf413bcdb9832750b83
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-06 21:57:40 +0500

    Add strings

---
+++ Strings.py
@@ -0,0 +1,20 @@
+# Strings are similar to arrays
+s = 'abc'
+print(s[0:2])
+
+# IT creates a new string
+s += 'efghk'
+print(s)
+
+# Strings can be converted into Integers
+a = (int('1234') + int('5678'))
+print(a)
+
+b = (str(123) + str(456))
+print(b)
+
+# If ASCIi value is needed
+print(ord('a'))
+print(ord('b'))
+print(ord('c'))
+print(ord('d'))
