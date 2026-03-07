47f39565cfe891a917548a0c6a029f3e0a48b748
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-07 22:30:48 +0500

    Add Hashset

---
+++ Hashset.py
@@ -0,0 +1,15 @@
+# Hash set
+
+my_set = set()
+my_set.add(1)
+my_set.add(2)
+my_set.add(3)
+print(my_set)
+print(f"Length of set is {len(my_set)}")
+
+print(1 in my_set)
+print(2 in my_set)
+print(3 in my_set)
+
+my_set.remove(2)
+print(my_set, 2 in my_set)
