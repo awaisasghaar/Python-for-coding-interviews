32c08a584ee19efeb6a17c753079691f8af4e531
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-07 22:20:08 +0500

    Add queues

---
+++ queues.py
@@ -0,0 +1,18 @@
+# Queues (double ended queues)
+from collections import deque
+
+queue = deque()
+queue.append(1)
+queue.append(2)
+print(queue)
+
+queue.popleft()
+print(queue)
+
+queue.appendleft(1)
+print(queue)
+
+queue.pop()
+print(queue)
+
+
