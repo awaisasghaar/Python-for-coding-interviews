0a4aeaa77e27541f1ec240949b1177b2f6d8cbf1
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-08 13:55:45 +0500

    Add Hash map

---
+++ Hash_Map.py
@@ -0,0 +1,48 @@
+# Hash map
+
+map = {}
+map['Awais'] = 'BC230200789'
+map['Umair'] = 'BC234566788'
+print(map)
+print(f'Length of map values are {len(map)}')
+
+# Changing the value
+map['Awais'] = 'BC230200788'
+print(map['Awais'])
+print(map)
+
+# checking if the key exists
+print('Awais' in map) # True
+print('Steve' in map) # False
+
+# We can also pop the Key
+map.pop('Umair')
+print(map)
+
+# Intialize the Hash map
+map = {'Awais': ('BC230200789'), 'Umair': ('BC234566788')}
+print(map)
+print(f'Length of map is {len(map)}')
+
+# Dict comprehension
+map = {i: 2*i for i in range(7)}
+print(map)
+
+# Loop through map
+map = {'Awais': 22, 'Umair': 23, 'Sufyan': 21}
+for key in map:
+    print(key, map[key])
+
+map = {'Awais': 22, 'Umair': 23, 'Sufyan': 21}
+for value in map.values():
+    print(value)
+
+for key, value in map.items():
+    print(key, value)
+
+
+
+
+
+
+
