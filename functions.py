a48e02de5a4c0a76f1d81c77726b999cba53bf52
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-10 19:13:26 +0500

    Changes have been made

--- functions.py
+++ functions.py
@@ -1,26 +1,31 @@
 # Functions in Python
 def match():
-
+    dict = {1: 'Pakistan', 2: 'New Zealand', 3: 'Australia', 4: 'South Africa'}
+    for key, value in dict.items():
+       print(key, value)
     n1 = input(f'Enter the team1 name: ')
     n2 = input(f'Enter the team2 name: ')
-    if n1.isalpha():
+
+    if n1 in dict.values():
         print(f'{n1} vs {n2}')
-    elif n2.isalpha():
-        print('Okay')
+    elif n2 in dict.values():
+        print(f'{n1} vs {n2}')
     else:
-        print('Invlaid name')
+        print(f'{n1} and {n2} are not in the dictionary')
     
 def players():
     try:
-        n = str(input('Would you like to tell the rankings? '))
-        if n == 'Yes' or n == 'yes':
-            print(f"Okay let's get started!")
-        elif  n == 'y' or n == 'Y':
-               print(f"Okay let's get started!")
-        elif  n == 'No' or n == 'no':
-               print(f'game is done')
+        n = str(input("What's your favorite teams: "))
+        if n == 'Pakistan' or n == 'PAKISTAN':
+            print(f"Ummm {n} are actualy not so good")
+        elif  n == 'New Zealand' or n == 'NEW ZEALAND':
+               print(f"Yeah! {n} are very good team")
+        elif  n == 'Australia' or n == 'australia':
+               print(f'{n} are world champions.')
+        elif  n == 'South Africa' or n == 'south africa':
+               print(f'{n} are world champions.')
         else:
-             print('Enter yes or no')
+             print(f'{n} is not in the dictionary')
 
     except ValueError:
         print('Enter valid inputs only.')
