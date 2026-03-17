21169382743bc431a536a5433a582455ac299a0a
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-17 23:29:26 +0500

    functions

--- Python Basics/functions.py
+++ Python Basics/functions.py
@@ -1,35 +1,35 @@
 # Functions in Python
+def fun():
+    a = input("\nEnter -> 'Hey or Hello': ")
+    name = str(input("Enter your name: "))
+    age = int(input("Enter your age: "))
 
-# def fun():
-#     a = input("\nEnter -> 'Hey or Hello': ")
-#     name = str(input("Enter your name: "))
-#     age = int(input("Enter your age: "))
+    if a == 'Hey':
+        print(f'{a}, My name is {name} and my age is {age}.')
+    elif a == 'Hello':
+        print(f'{a}, My name is {name} and my age is {age}.')
+    else:
+        print(f"{a} is not valid")
 
-#     if a == 'Hey':
-#         print(f'{a}, My name is {name} and my age is {age}.')
-#     elif a == 'Hello':
-#         print(f'{a}, My name is {name} and my age is {age}.')
-#     else:
-#         print(f"{a} is not valid")
+def ask():
+    print("\nEnter Yes or No")
+    b = input("Would you like to answer the questions?: ")
+    if b == 'Yes' or b == 'yes':
+        answer = input('Are you sure?: ')
+        if answer in ('Yes', 'yes'):
+            print("Ok Great! let's proceed")
+        elif answer in ('No', 'no'):
+            print('Ok see you next time.')
+    elif b == 'No' or b == 'no':
+        print("Ok, we won't ask questions to you.")
 
-# def ask():
-#     print("\nEnter Yes or No")
-#     b = input("Would you like to answer the questions?: ")
-#     if b == 'Yes' or b == 'yes':
-#         answer = input('Are you sure?: ')
-#         if answer in ('Yes', 'yes'):
-#             print("Ok Great! let's proceed")
-#         elif answer in ('No', 'no'):
-#             print('Ok see you next time.')
-#     elif b == 'No' or b == 'no':
-#         print("Ok, we won't ask questions to you.")
-    
-# def cal():
-#     a = int(input("\nWhat's the Number: "))
-#     for i in range(1, 11):
-#        print(f"{a} * {i} = {a * i}")
+# Tables
+def cal():
+    a = int(input("\nWhat's the Number: "))
+    for i in range(1, 11):
+       print(f"{a} * {i} = {a * i}")
 
-
+# Student marks sheet
 def Student_details():
     print("\n"
     "  --------- Marks Sheet ---------  ")
@@ -132,9 +132,9 @@ def total(total_cal_1, total_cal_2, total_cal_3, total_marks):
 
 
 if __name__ == '__main__':
-#    fun()
-#    ask()
-#    cal()
+   fun()
+   ask()
+   cal()
    Student_details()
    total_cal_1, total_cal_2, total_cal_3, total_marks = marks()
    total(total_cal_1, total_cal_2, total_cal_3, total_marks)
