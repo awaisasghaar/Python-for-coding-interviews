38e3051f2748c899dfddc45bcee8b8a1532ca552
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-25 16:36:48 +0500

    Add more tests

--- Python Basics/functions_test.py
+++ Python Basics/functions_test.py
@@ -1,5 +1,5 @@
 import pytest
-from functions import sheet, percentage
+from functions import sheet, percentage, total_subject_marks
 
 def test_integer_sheet():
     subjects = {'Maths': 88, 'Computer Science': 52, 'English': 65}
@@ -10,6 +10,19 @@ def test_integer_sheet():
 def test_percentage():
     assert percentage(230) == 'Grade B+ (Pass)'
     assert percentage(143) == 'Fail'
+    assert percentage(250) == 'Grade A (Pass)'
+
+def test_total_subject_marks():
+    subjects = {'Maths': 88, 'Computer Science': 52, 'English': 65}
+    assert total_subject_marks(subjects) == 205
+
+def test_total_subject_marks_zaro():
+    subjects = {'Maths': 0, 'Computer Science': 0, 'English': 0}
+    assert total_subject_marks(subjects) == 0
+
+
+
+
 
 
 
